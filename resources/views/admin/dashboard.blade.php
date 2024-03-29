af274244c67ff6f7eccef26763e06f4d5bb7ca55
atp-aung <atp.aung.5555@gmail.com>
2024-03-29 21:35:17 +0630

    d

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -19,46 +19,41 @@
     </div>
 </div>
 
-<div class="card">   
-    <div class="card-title"> 
-<a class="btn btn-secondary"
-href="{{route('products.add')}}">
-+Add Product
-</a>
-
-<a class="btn btn-secondary"
-href="{{route('dashboardSub')}}">
-Product Management
-</a>
-
-<a class="btn btn-secondary"
-href="{{route('orderMgmtView')}}">
-Order Management
-</a>
-</div>
-
-<h1 class="alert alert-primary">Product List</h1>
-
-<ul >
-@foreach($products as $product)
-<li class="card">    
-    <div class="card-body">
-       <div class="alert alert-secondary">    
-        product: {{ $product['product_name'] }} ---- price: {{ $product['price'] }}
-        </div>
-        <a class="btn btn-warning"
-        href="{{route('products.edit', ['id' => $product->id])}}">
-        Edit
+<div class="card">
+    <div class="card-title">
+        <a class="btn btn-secondary" href="{{route('products.add')}}">
+            +Add Product
         </a>
 
-        <a class="btn btn-danger"
-        href="{{route('products.delete', ['id' => $product->id])}}">
-        Delete
+        <a class="btn btn-secondary" href="{{route('dashboardSub')}}">
+            Product Management
+        </a>
+
+        <a class="btn btn-secondary" href="{{route('orderMgmtView')}}">
+            Order Management
         </a>
     </div>
-</li>
-<br>
-@endforeach
-</ul>
+
+    <h1 class="alert alert-primary">Product List</h1>
+
+    <ul>
+        @foreach($products as $product)
+        <li class="card">
+            <div class="card-body">
+                <div class="alert alert-secondary">
+                    product: {{ $product['product_name'] }} ---- price: {{ $product['price'] }}
+                </div>
+                <a class="btn btn-warning" href="{{route('products.edit', ['id' => $product->id])}}">
+                    Edit
+                </a>
+
+                <a class="btn btn-danger" href="{{route('products.delete', ['id' => $product->id])}}">
+                    Delete
+                </a>
+            </div>
+        </li>
+        <br>
+        @endforeach
+    </ul>
 </div>
 @endsection
