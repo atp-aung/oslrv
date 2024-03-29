0854fa655c5bddf660b8f13888ea5c16d3e9719b
atp-aung <atp.aung.5555@gmail.com>
2024-03-29 21:38:19 +0630

    v

--- resources/views/admin/orderMgmt.blade.php
+++ resources/views/admin/orderMgmt.blade.php
@@ -2,61 +2,59 @@
 @section('content')
 <div class="card">
     <div class="card-title">
-        <a class="btn btn-secondary"
-        href="{{route('dashboardSub')}}">
-        Product Management
+        <a class="btn btn-secondary" href="{{route('dashboardSub')}}">
+            Product Management
         </a>
-        <a class="btn btn-secondary"
-        href="{{route('orderMgmtView')}}">
-        Order Management
+        <a class="btn btn-secondary" href="{{route('orderMgmtView')}}">
+            Order Management
         </a>
     </div>
 
-<h1 class="alert alert-primary">Orders</h1>
-<ul>
-@foreach($orders as $order)
-    <li class="card">
-        <div class="card-body">    
-            <div class="alert alert-secondary">
-            Order ID: {{ $order->id }} 
-            <br>
-            Customer Name: {{ $order->customer_name }} 
-            <br>
-            Address: {{$order->address}} 
-            <br>
-            deli_status: {{ $order->deli_status ? "done" : "not yet delivered" }}    
-            </div>
-            <h3>Order Items</h3>
-            <ol>
-                
-            @foreach ($order->orderItems as $item)
-            <li>
-            Product: {{ optional($item->product)->product_name }} 
-            <br>
-            Quantity: {{ $item->quantity }}
-            </li>
-            @endforeach
-            <br>
-            
-            @if($order->deli_status == 0)
-            <form action="{{ route('statusChange',['id'=> $order->id]) }}" method="POST">
-            @csrf
-            @method('PUT')
-            <button class="btn btn-warning" type="submit" name="deli_status" value=1>Mark as Delivered</button>
-            </form>
-            @elseif($order->deli_status == 1)
-            <form action="{{ route('statusChange', ['id'=>$order->id]) }}" method="POST">
-            @csrf
-            @method('PUT')
-            <button class="btn btn-info" type="submit" name="deli_status" value=0>Undo</button>
-            </form>
-            @endif
+    <h1 class="alert alert-primary">Orders</h1>
+    <ul>
+        @foreach($orders as $order)
+        <li class="card">
+            <div class="card-body">
+                <div class="alert alert-secondary">
+                    Order ID: {{ $order->id }}
+                    <br>
+                    Customer Name: {{ $order->customer_name }}
+                    <br>
+                    Address: {{$order->address}}
+                    <br>
+                    deli_status: {{ $order->deli_status ? "done" : "not yet delivered" }}
+                </div>
+                <h3>Order Items</h3>
+                <ol>
 
-            </ol>
-        </div>
-    </li>
-    <br>
-@endforeach
-</ul>
+                    @foreach ($order->orderItems as $item)
+                    <li>
+                        Product: {{ optional($item->product)->product_name }}
+                        <br>
+                        Quantity: {{ $item->quantity }}
+                    </li>
+                    @endforeach
+                    <br>
+
+                    @if($order->deli_status == 0)
+                    <form action="{{ route('statusChange',['id'=> $order->id]) }}" method="POST">
+                        @csrf
+                        @method('PUT')
+                        <button class="btn btn-warning" type="submit" name="deli_status" value=1>Mark as Delivered</button>
+                    </form>
+                    @elseif($order->deli_status == 1)
+                    <form action="{{ route('statusChange', ['id'=>$order->id]) }}" method="POST">
+                        @csrf
+                        @method('PUT')
+                        <button class="btn btn-info" type="submit" name="deli_status" value=0>Undo</button>
+                    </form>
+                    @endif
+
+                </ol>
+            </div>
+        </li>
+        <br>
+        @endforeach
+    </ul>
 </div>
 @endsection
--- resources/views/admin/productAdd.blade.php
+++ resources/views/admin/productAdd.blade.php
@@ -3,18 +3,17 @@
 
 <h1>Product add form</h1>
 <div class="container">
-<form method="post">
-@csrf
-<div class="mb-3">
-<label>Product name</label>
-<input type="text" name="product_name"  class="form-control">
-</div>
-<div class="mb-3">
-<label>Price</label>
-<input type="number" name="price" class="form-control">
-</div>
-<input type="submit" value="Create"
-class="btn btn-primary">
-</form>
+    <form method="post">
+        @csrf
+        <div class="mb-3">
+            <label>Product name</label>
+            <input type="text" name="product_name" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label>Price</label>
+            <input type="number" name="price" class="form-control">
+        </div>
+        <input type="submit" value="Create" class="btn btn-primary">
+    </form>
 </div>
 @endsection
